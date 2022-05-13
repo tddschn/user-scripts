38fc20e5316d1cccccd7f6ecbb3f6f8700842bd5
Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-05-13 09:56:24 +0800

    2022-05-13T09-56-24Z

---
+++ wjx-report-editor/wjx-report-editor.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         wjx report editor
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Edit your wjx.cn reports
+// @license      MIT
+// @author       tddschn
+// @match        https://www.wjx.cn/report/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=wjx.cn
+// @grant        none
+// @updateURL    https://github.com/tddschn/user-scripts/raw/master/wjx-report-editor/wjx-report-editor.meta.js
+// @downloadURL  https://github.com/tddschn/user-scripts/raw/master/wjx-report-editor/wjx-report-editor.user.js
+// ==/UserScript==
+
