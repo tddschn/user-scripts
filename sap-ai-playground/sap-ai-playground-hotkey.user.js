b159f8cacff55f730f654b69092730909337bc18
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-04-14 21:46:12 +0800

    2023-04-14T21-46-12Z

---
+++ sap-ai-playground/sap-ai-playground-hotkey.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Trigger Button Click on Cmd/Ctrl-Enter
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Click the button with ID btnChat when the user presses Cmd-Enter or Ctrl-Enter
+// @author       ChatGPT, tddschn
+// @match        https://ai-playground.cfapps.sap.hana.ondemand.com/index.html
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function triggerButtonClick(e) {
+        // Check if the pressed key is Enter and either Cmd or Ctrl is also pressed
+        if (e.key === 'Enter' && (e.ctrlKey || e.metaKey)) {
+            const button = document.getElementById("btnChat");
+            if (button) {
+                button.click();
+            }
+        }
+    }
+
+    // Add the event listener to the document
+    document.addEventListener('keydown', triggerButtonClick);
+})();
